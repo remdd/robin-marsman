49cda26ed82f125ae1e8fc35a50487c114e99100
Robin Marsden <munkrat@gmail.com>
2026-01-20 23:24:06 +0000

    Update homepage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div
-      className="min-h-screen flex items-start justify-center pt-[30vh] bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url(/img/mars/mars-1.jpg)" }}
-    >
-      <h1 className="text-6xl font-bold text-white tracking-widest">
+    <div className="min-h-screen flex items-start justify-center pt-[30vh] bg-[url('/img/mars/mars-1.jpg')] bg-cover bg-center bg-no-repeat relative before:absolute before:inset-0 before:bg-black before:opacity-50">
+      <h1 className="text-6xl font-bold text-white tracking-widest relative z-10">
         robin marsman
       </h1>
     </div>
