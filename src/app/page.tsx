bbee4fcdda30de512f14412c86f1e7b31a63dc75
Robin Marsden <munkrat@gmail.com>
2026-01-21 11:15:50 +0000

    Add Mars images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center relative bg-black">
       <Image
-        src={getAssetPath("/img/mars/mars-1.jpg")}
+        src={getAssetPath("/img/mars/1.jpg")}
         alt="Mars background"
         fill
         priority
