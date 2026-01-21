7d2beca2527d4750ba0e60c6f1015383b29589a8
Robin Marsden <munkrat@gmail.com>
2026-01-21 10:49:16 +0000

    Content updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 import { basePath, getAssetPath } from "@/utils/paths";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className="min-h-screen flex items-start justify-center pt-[30vh] bg-cover bg-center bg-no-repeat relative before:absolute before:inset-0 before:bg-black before:opacity-50 shadow-[0_0_50px_rgba(0,0,0,0.9)_inset]"
-      style={{
-        backgroundImage: `url("${getAssetPath("/img/mars/mars-1.jpg")}")`,
-      }}
-    >
-      <div className="flex flex-col items-center relative z-10">
-        <h1 className="text-6xl font-bold text-white tracking-widest">
-          robin marsman
-        </h1>
-        <div className="h-1 w-[60%] bg-white my-16" />
-        <p className="text-lg text-white">
-          Debut album <strong>"Red world dawning"</strong> coming soon
-        </p>
+    <div className="min-h-screen flex items-center justify-center relative bg-black">
+      <Image
+        src={getAssetPath("/img/mars/mars-1.jpg")}
+        alt="Mars background"
+        fill
+        priority
+        className="object-cover contrast-125 saturate-100 hue-rotate-350"
+        style={{ zIndex: 0 }}
+      />
+      <div className="absolute inset-0 bg-black opacity-30 z-[1]" />
+      <div className="absolute inset-0 z-[2] shadow-[inset_0_0_100px_50px_rgba(0,0,0,0.4)]" />
+      <div>
+        <div className="flex flex-col items-center relative z-10 text-center">
+          <h1 className="text-6xl font-bold text-white tracking-widest">
+            robin marsman
+          </h1>
+          <div className="h-1 w-[60%] bg-white my-16" />
+          <p className="py-4 px-8 bg-white text-lg">
+            Debut album <strong>"Red world dawning"</strong> coming soon
+          </p>
+        </div>
       </div>
     </div>
   );
