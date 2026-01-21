8360e7cd7c70368c1dd3a4b156498d44dd8c88fe
Robin Marsden <munkrat@gmail.com>
2026-01-21 21:50:43 +0000

    Tweak vignette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center relative bg-black">
       <MarsCarousel />
       <div className="absolute inset-0 bg-black opacity-30 z-[1]" />
-      <div className="absolute inset-0 z-[2] shadow-[inset_0_0_100px_50px_rgba(0,0,0,0.4)]" />
+      <div className="absolute inset-0 z-[2] shadow-[inset_0_0_250px_100px_rgba(0,0,0,0.4)]" />
       <div>
         <div className="flex flex-col items-center relative z-10 text-center">
           <h1 className="text-6xl font-bold text-white tracking-widest">
