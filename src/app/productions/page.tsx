de88b747175899624b94f67c8dfce7f54494f9ec
Robin Marsden <robin@kyan.com>
2026-02-17 16:05:40 +0000

    Update productions page to import album URL from config

--- src/app/productions/page.tsx
+++ src/app/productions/page.tsx
@@ -3,6 +3,10 @@ import { PageContainer } from "@/components/PageContainer";
 import { H1 } from "@/components/H1";
 import { Textbox } from "@/components/Textbox/Textbox";
 import { createPageMetadata } from "@/lib/metadata";
+import { TextLink } from "@/components/TextLink/TextLink";
+import { PhotoImage } from "@/components/PhotoImage/PhotoImage";
+import { albumLinks } from "@/config";
+import redWorldDawningImage from "@public/img/robin-marsman---red-world-dawning.jpg";
 
 export const metadata: Metadata = createPageMetadata("productions");
 
@@ -11,12 +15,31 @@ export default function Productions() {
     <PageContainer>
       <H1>productions</H1>
       <Textbox>
-        Debut album <strong>"red world dawning"</strong> coming soon
+        Debut album <strong>"red world dawning"</strong> out now on{" "}
+        <TextLink
+          href={albumLinks.redWorldDawning}
+          external
+          underline
+          theme="dark"
+        >
+          Bandcamp
+        </TextLink>
+        !
         <br />
         <br />
-        <span className="text-red-600">
-          Bandcamp release <strong>Friday 20th Feb 2026</strong>!
-        </span>
+        <div className="flex flex-col items-center">
+          <a
+            href={albumLinks.redWorldDawning}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <PhotoImage
+              src={redWorldDawningImage}
+              alt="Album cover for Red World Dawning by Robin Marsman"
+              className="w-160"
+            />
+          </a>
+        </div>
       </Textbox>
     </PageContainer>
   );
