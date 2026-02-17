2618d9ce51ecb97934ba5ea141db889e38d87b93
Robin Marsden <robin@kyan.com>
2026-02-17 16:03:00 +0000

    Add src/config/links.ts with external URLs

---
+++ src/config/links.ts
@@ -0,0 +1,33 @@
+/**
+ * External links configuration
+ * Single source of truth for all external URLs
+ */
+
+/** Social media profile URLs */
+export const socialLinks = {
+  bandcamp: "https://robinmarsman.bandcamp.com/",
+  mixcloud: "https://www.mixcloud.com/robinmarsman/",
+  facebook: "https://www.facebook.com/people/Robin-Marsman/61587173637583/",
+  email: "mailto:robinmarsman@proton.me",
+} as const;
+
+/** Album/release URLs */
+export const albumLinks = {
+  redWorldDawning: "https://robinmarsman.bandcamp.com/album/red-world-dawning",
+} as const;
+
+/** Mixcloud embed widget URLs */
+export const mixcloudEmbeds = {
+  blobbysPlagueWeekender:
+    "https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Frobinmarsman%2Fdj-set-recorded-live-blobbys-creamy-plague-weekender-2021%2F",
+  redMarsRadio0:
+    "https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Frobinmarsman%2Fred-mars-radio-0%2F",
+  polycyclicBisturbilism:
+    "https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Frobinmarsman%2Fpolycyclic-bisturbilism%2F",
+  covidcoreFishHeadband:
+    "https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Frobinmarsman%2Fcovidcore-fish-headband%2F",
+} as const;
+
+export type SocialLinkKey = keyof typeof socialLinks;
+export type AlbumLinkKey = keyof typeof albumLinks;
+export type MixcloudEmbedKey = keyof typeof mixcloudEmbeds;
