336a2b45263abc21753103ac558d3c5236aaf2e4
Robin Marsden <robin@kyan.com>
2026-02-17 16:03:15 +0000

    Add src/config/index.ts barrel export

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+export { siteConfig, type SiteConfig } from "./site";
+export { routes, navItems, type Route, type RouteKey } from "./routes";
+export {
+  socialLinks,
+  albumLinks,
+  mixcloudEmbeds,
+  type SocialLinkKey,
+  type AlbumLinkKey,
+  type MixcloudEmbedKey,
+} from "./links";
